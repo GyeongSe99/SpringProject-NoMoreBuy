c27cb30dcd7e51f210df07ee7f53a2a919d75675
GyeongSe99 <sgcheon@naver.com>
2023-08-20 22:03:35 +0900

    Initial Setting

---
+++ src/main/java/com/zerobase/nomorebuy/NomorebuyApplication.java
@@ -0,0 +1,13 @@
+package com.zerobase.nomorebuy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NomorebuyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NomorebuyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zerobase/nomorebuy/NomorebuyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zerobase.nomorebuy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NomorebuyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
